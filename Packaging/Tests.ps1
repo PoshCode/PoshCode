3d4e306bafa417df438a77606f4f56c0ad76179c
Joel Bennett <Jaykul@HuddledMasses.org>
2013-04-14 01:06:50 -0400

    Write up some TODO stuff ... 'cause I'm loosing track

--- Packaging/Tests.ps1
+++ Packaging/Tests.ps1
@@ -1,5 +1,3 @@
-## Test Cases Needed:
-
 # File System Install
 Test "Install a new module with no dependencies from a file (clean install)" { assert { throw "Test Not Written" }} -Category "FileSystem"
 Test "Install an upgrade module from a file (should overwrite module files but not delete ini files)" { assert { throw "Test Not Written" }} -Category "FileSystem"
@@ -16,3 +14,16 @@ Test "Install a module with dependencies from a url with dependency package is a
 Test "Install a module with dependencies from a url with dependency ReleaseUri set correctly (should download to same place and install)" { assert { throw "Test Not Written" }} -Category "Download"
 Test "Install a module with dependencies from a url with dependencies that have unreachable ReleaseUri (should fail cleanly!)" { assert { throw "Test Not Written" }} -Category "Download"
 
+# Get Module Info
+Test "Validate the error message when getting module info from a module with no .psd1" { assert { throw "Test Not Written" } -Category "ModuleInfo"
+Test "Get Module Info from an installed module with just .psd1" { assert { throw "Test Not Written" } -Category "ModuleInfo"
+Test "Get Module Info from an installed module with a .moduleinfo" { assert { throw "Test Not Written" } -Category "ModuleInfo"
+Test "Get Module Info from a local .psmx package file." { assert { throw "Test Not Written" } -Category "ModuleInfo"
+Test "Get Module Info from a remote (UNC) package file." { assert { throw "Test Not Written" } -Category "ModuleInfo"
+Test "Get Module Info from a remote (http) package file." { assert { throw "Test Not Written" } -Category "ModuleInfo"
+
+# Update Module Info
+Test "Update Module Info when there are new things in the .psd1" { assert { throw "Test Not Written" } -Category "UpdateModuleInfo","ModuleInfo"
+Test "Update Module Info when there are things removed from the .psd1 (not supported yet)" { assert { throw "Test Not Written" } -Category "UpdateModuleInfo","ModuleInfo"
+Test "Update Module Info when the .moduleinfo file hasn't been created yet" { assert { throw "Test Not Written" } -Category "UpdateModuleInfo","ModuleInfo"
+Test "Update Module Info with various parameters" { assert { throw "Test Not Written" } -Category "UpdateModuleInfo","ModuleInfo"
