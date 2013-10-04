be61fd3666585e479ad239f7fc0351db19f82d79
Joel Bennett <Jaykul@HuddledMasses.org>
2013-10-04 01:28:09 -0400

    Delete source code for xaml serialization (we're moving to .psd1)

--- Source/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Reflection;
-
-[assembly: System.Windows.Markup.XmlnsDefinition("http://schemas.poshcode.org/packaging", "PoshCode.Packaging")]
-[assembly: System.Windows.Markup.XmlnsDefinition("http://schemas.microsoft.com/generics", "System.Collections.Generic")]
-
